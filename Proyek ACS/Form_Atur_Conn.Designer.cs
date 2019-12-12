999ba60653c96a5cd4fda21184ea1132c16fa7d4
Gracielo Justine <gracielo1@mhs.stts.edu>
2019-12-12 21:21:35 +0700

    Tambah Form awal untuk atur conn(masih error)

---
+++ Proyek ACS/Form_Atur_Conn.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Proyek_ACS
+{
+    partial class Form_Atur_Conn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.txt_uname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(168, 164);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.PasswordChar = '*';
+            this.txt_password.Size = new System.Drawing.Size(152, 20);
+            this.txt_password.TabIndex = 17;
+            // 
+            // txt_uname
+            // 
+            this.txt_uname.Location = new System.Drawing.Point(167, 126);
+            this.txt_uname.Name = "txt_uname";
+            this.txt_uname.Size = new System.Drawing.Size(152, 20);
+            this.txt_uname.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(83, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Password : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(80, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Username : ";
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btn_login.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.Location = new System.Drawing.Point(160, 217);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(105, 54);
+            this.btn_login.TabIndex = 13;
+            this.btn_login.Text = "SET";
+            this.btn_login.UseVisualStyleBackColor = false;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(168, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
+            this.textBox1.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(116, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 16);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Host :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(123, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(162, 31);
+            this.label5.TabIndex = 45;
+            this.label5.Text = "Connection";
+            // 
+            // Form_Atur_Conn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 283);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_uname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_login);
+            this.Name = "Form_Atur_Conn";
+            this.Text = "Form_Atur_Conn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.TextBox txt_uname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Proyek ACS/Form_Atur_Conn.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace Proyek_ACS
+{
+    public partial class Form_Atur_Conn : Form
+    {
+        public static OracleConnection conn;
+        string host, db = "XE", user, pass;
+        public awal a;
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            bool cek=true;
+            if (textBox1.Text=="")
+            {
+                cek = false;
+            }
+            if (txt_uname.Text == "")
+            {
+                cek = false;
+            }
+            if (txt_password.Text == "")
+            {
+                cek = false;
+            }
+            if (cek)
+            {
+                host = textBox1.Text;
+                user = txt_uname.Text;
+                pass = txt_password.Text;
+                conn = new OracleConnection("Data Source=" +
+                   "(DESCRIPTION=" +
+                   "(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)" +
+                   "(HOST=" + host + ")(PORT=1521)))" +
+                   "(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=" + db + ")));" +
+                   "user id=" + user + ";password=" + pass);
+                conn.Close();
+                conn.Open();
+                conn.Close();
+                awal.conn = conn;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Data Tidak Lengkap");
+            }
+        }
+
+        public Form_Atur_Conn()
+        {
+            InitializeComponent();
+           
+        }
+    }
+}
